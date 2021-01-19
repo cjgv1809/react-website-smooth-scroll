79bf8530efc601367e66aa8941b636b3ad797f65
Carlos <carlos_gomes1809@hotmail.com>
2021-01-19 13:11:14 -0300

    basename prop added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SigninPage from "./pages/SigninPage";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
